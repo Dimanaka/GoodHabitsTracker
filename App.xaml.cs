af2dd5e33d5a48822a41aa2ffc8c084c146baa7e
Dmytro <dimayovenko40@gmail.com>
2025-05-15 19:05:06 +0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HabitsTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
